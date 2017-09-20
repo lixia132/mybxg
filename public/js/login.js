ff3008481c9d39b7db3e28c7f87bd7c6f6550474
lixia <2873862456@qq.com>
2017-09-20 11:24:05 +0800

    模块化重构项目结构

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+define(['jquery','cookie'],function($){
+	   $('#loginBtn').click(function(){
+            $.ajax({
+                type:'post',
+                url:'/api/login',
+                data:$('#loginForm').serialize(),
+                dataType:'json',
+                success:function(data){
+                    if(data.code==200){
+                        $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
+                        location.href='/main/index';
+                    }
+                }
+            })
+
+
+
+                return false;
+        });
+})
