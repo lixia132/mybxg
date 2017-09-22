196524881a7ffd4fd3d710319b73ca8166a01f0a
lixia <2873862456@qq.com>
2017-09-22 09:51:56 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,41 @@
 define(['jquery','template'],function($,template){
+	//调用接口获取所有的讲师数据
 	$.ajax({
 		type:'get',
 		url:'/api/teacher',
 		dataType:'json',
 		success:function(data){
-			console.log(data);
+			// console.log(data);
 			var html=template('teacherTpl',{list:data.result});
 			$('#teacherInfo').html(html);
+
+			//启用注销功能
+			$('.eod').click(function(){
+				// console.log(123);
+				var that=this;
+				var td=$(this).closest('td');
+				var tcId=td.attr('data-tcId');
+				var status=td.attr('data-status');
+				// console.log(tcId,status);
+				$.ajax({
+					type:'post',
+					url:'/api/teacher/handle',
+					data:{tc_id:tcId,tc_status:status},
+					dataType:'json',
+					success:function(data){
+						// console.log(data);
+						if(data.code==200){
+							td.attr('data-status','data.result.tc_status');
+							if(data.result.tc_status==0){
+								$(that).text('注销');
+							}else{
+								$(that).text('启用');
+							}
+						}
+
+					}
+				})
+			});
 		}
 	});
 });
