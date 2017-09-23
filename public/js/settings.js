869d3db1bf4765566deecf38f110e0643abecd97
lixia <2873862456@qq.com>
2017-09-23 12:32:05 +0800

    个人中心页面展示

---
+++ public/js/settings.js
@@ -0,0 +1,12 @@
+define(['jquery','template'],function($,template){
+  //调用接口获取个人信息
+  $.ajax({
+    type:'get',
+    url:'/api/teacher/profile',
+    dataType:'json',
+    success:function(data){
+      var html=template('settingsTpl',data.result);
+     $('#settingsInfo').html(html);
+    }
+  });
+});
