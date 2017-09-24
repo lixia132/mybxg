ec4a52a84b7865a27ba94857b02c17eeb521f495
lixia <2873862456@qq.com>
2017-09-24 19:31:59 +0800

    完成省市县三级联动和图片上传

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','uploadify','region'],function($,template){
   //调用接口获取个人信息
   $.ajax({
     type:'get',
@@ -7,6 +7,24 @@ define(['jquery','template'],function($,template){
     success:function(data){
       var html=template('settingsTpl',data.result);
      $('#settingsInfo').html(html);
+     //处理头像上传
+     $('#upfile').uploadify({
+       width:120,
+       height:120,
+       buttonText:'',
+       itemTemplate:'<span></span>',
+        swf:'/public/assets/uploadify/uploadify.swf',
+        uploader:'/api/uploader/avatar',
+        fileObjName:'tc_avatar',
+        onUploadSuccess:function(a,b){
+         var obj=JSON.parse(b);
+         $('.preview img').attr('src',obj.result.path);
+        }
+     });
+     //处理省市县三级联动
+     $('#pcd').region({
+       url:'/public/assets/jquery-region/region.json'
+     });
     }
   });
 });
