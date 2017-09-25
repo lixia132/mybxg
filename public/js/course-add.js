869e2cfe2dd991e9cd532c5cb11f045d0478aaa6
lixia <2873862456@qq.com>
2017-09-25 19:57:33 +0800

    完成课程添加和编辑的页面共用

---
+++ public/js/course-add.js
@@ -0,0 +1,17 @@
+define(['jquery','util','form'],function($,util){
+    util.setMenu(location.pathname);
+
+    //绑定表单提交单击事件
+    $('#courseBtn').click(function(){
+      $('#courseForm').ajaxSubmit({
+        type:'post',
+        url:'/api/course/create',
+        dataType:'json',
+        success:function(data){
+          if(data.code==200){
+            location.href='/course/basic?cs_id='+data.result.cs_id;
+          }
+        }
+      });
+    });
+});
